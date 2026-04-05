95c3315fb8f6eae722772dd03887137d264a3803
Alexlink10 <alecsandrolink2@hotmail.com>
2026-04-05 19:00:02 -0300

    commit ok

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,26 +1,24 @@
+import 'dotenv/config';
 import 'reflect-metadata';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { AppDataSource } from '../typeorm/migrations/data-source';
 import routes from './routes';
 import cors from 'cors';
 
 const app = express();
-const PORT = 3000;
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-  }),
-);
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
 AppDataSource.initialize()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(' Servidor rodando na porta 3000');
+      console.log(`Servidor rodando na porta ${PORT}`);
     });
   })
   .catch(error => console.log('Erro ao conectar no banco:', error));
-
 /*
 app.get("/", (req: Request, res: Response) => {
   res.send("Servidor rodando!");
