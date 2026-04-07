c551042af53397b1de7478b11fc3ed43b6573120
Alexlink10 <alecsandrolink2@hotmail.com>
2026-04-07 11:34:00 -0300

    listOrde alterada

--- src/modules/orders/services/ListOrdersService.ts
+++ src/modules/orders/services/ListOrdersService.ts
@@ -3,12 +3,18 @@ import Orders from '../typeorm/entities/Orders';
 
 class ListOrdersService {
   public async execute(): Promise<Orders[]> {
-    const ordersRepository = AppDataSource.getRepository(Orders);
+    try {
+      const ordersRepository = AppDataSource.getRepository(Orders);
 
-    const orders = await ordersRepository.find({
-      relations: ['photos', 'problems'],
-    });
-    return orders;
+      const orders = await ordersRepository.find({
+        relations: ['photos'],
+      });
+
+      return orders;
+    } catch (error) {
+      console.log('ERRO AQUI >>>>>>', error);
+      throw error;
+    }
   }
 }
 
